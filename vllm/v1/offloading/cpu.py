09100ac6f2f9f4d896859071b447f0af09c942c8
Or Ozeri <oro@il.ibm.com>
2025-08-03 07:26:26 +0300

    Add CPU offloading support
    This commit registers a new OffloadingSpec to add CPU offloading support
    to the OffloadingConnector.
    
    Signed-off-by: Or Ozeri <oro@il.ibm.com>

--- vllm/v1/offloading/cpu.py
+++ vllm/v1/offloading/cpu.py
@@ -1,9 +1,21 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 import ctypes
+from collections.abc import Iterator
+from typing import Optional
 
-from vllm.v1.offloading.lru_manager import Backend, BlockStatus
-from vllm.v1.offloading.mediums import CPULoadStoreSpec
+import torch
+
+from vllm.attention import get_attn_backend
+from vllm.config import VllmConfig
+from vllm.platforms import current_platform
+from vllm.v1.offloading.abstract import LoadStoreSpec, OffloadingManager
+from vllm.v1.offloading.lru_manager import (Backend, BlockStatus,
+                                            LRUOffloadingManager)
+from vllm.v1.offloading.mediums import CPULoadStoreSpec, GPULoadStoreSpec
+from vllm.v1.offloading.spec import OffloadingSpec
+from vllm.v1.offloading.worker.cpu_gpu import CpuGpuOffloadingHandler
+from vllm.v1.offloading.worker.worker import OffloadingHandler
 
 
 class CPUBlockStatus(BlockStatus):
@@ -54,3 +66,64 @@ class CPUBackend(Backend):
     def free(self, block: BlockStatus):
         assert isinstance(block, CPUBlockStatus)
         self.allocated_blocks_free_list.append(block.block_id)
+
+
+class CPUOffloadingSpec(OffloadingSpec):
+
+    def __init__(self, vllm_config: VllmConfig):
+        super().__init__(vllm_config)
+
+        self._num_cpu_blocks: Optional[int] = None
+
+        # scheduler-side
+        self._manager: Optional[OffloadingManager] = None
+
+        # worker-side
+        self._handler: Optional[OffloadingHandler] = None
+
+    @property
+    def num_cpu_blocks(self):
+        if self._num_cpu_blocks is None:
+            self._num_cpu_blocks = self.extra_config.get("num_cpu_blocks")
+            if not self._num_cpu_blocks:
+                raise Exception("num_cpu_blocks must be specified "
+                                "in kv_connector_extra_config")
+        return self._num_cpu_blocks
+
+    def get_manager(self) -> OffloadingManager:
+        if not self._manager:
+            kv_events_config = self.vllm_config.kv_events_config
+            enable_events = (kv_events_config is not None
+                             and kv_events_config.enable_kv_cache_events)
+            self._manager = LRUOffloadingManager(CPUBackend(
+                block_size=self.offloaded_block_size,
+                num_blocks=self.num_cpu_blocks),
+                                                 enable_events=enable_events)
+        return self._manager
+
+    def get_handlers(
+        self, kv_caches: dict[str, torch.Tensor]
+    ) -> Iterator[tuple[type[LoadStoreSpec], type[LoadStoreSpec],
+                        OffloadingHandler]]:
+        if not self._handler:
+            if not current_platform.is_cuda():
+                raise Exception("CPU Offloading is currently only supported"
+                                " on CUDA GPUs")
+            attn_backend = get_attn_backend(
+                self.vllm_config.model_config.get_head_size(),
+                self.vllm_config.model_config.dtype,
+                self.vllm_config.cache_config.cache_dtype,
+                self.gpu_block_size,
+                self.vllm_config.model_config.is_attention_free,
+                use_mla=self.vllm_config.model_config.use_mla)
+
+            self._handler = CpuGpuOffloadingHandler(
+                attn_backend=attn_backend,
+                gpu_block_size=self.gpu_block_size,
+                cpu_block_size=self.offloaded_block_size,
+                num_cpu_blocks=self.num_cpu_blocks,
+                gpu_caches=list(kv_caches.values()))
+
+        assert self._handler is not None
+        yield GPULoadStoreSpec, CPULoadStoreSpec, self._handler
+        yield CPULoadStoreSpec, GPULoadStoreSpec, self._handler
